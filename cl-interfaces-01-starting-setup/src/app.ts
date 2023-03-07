c287227937939d62fde22c557db53105a770888a
albertonaval <albertonaval.dev@gmail.com>
2023-03-07 18:54:57 +0100

    Interfaces Done

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,122 +1,62 @@
-abstract class Department {
 
-    static fiscalYear = 2020
+// type AddFn = (a: number, b: number) => number
 
-    constructor(protected readonly id: string, protected name: string, protected people : string[] = []) { }
-    //Se pasan los argumentos directamente junto con el tipo de dato y la caracteristica.
-
-    abstract describe(this: Department): void
-    //pasarle el This con el tipo de dato como parametro aumenta la seguridad e indica que el metodo describe solo puede ser referido a la clase a la que pertenece
-
-    addEmployees(employe: string) {
-        this.people.push(employe)
-    }
-
-    printEmployeeInfo() {
-        console.log(this.people.length)
-        console.log(this.people)
-    }
+interface AddFn {
+    (a: number, b: number) : number
 }
 
-class ITDepartment extends Department {
-    admins: string[]
-    constructor(id: string, admins: string[]) {
-        super(id, 'IT')
-        this.admins = admins
+let add: AddFn
 
-    }
+add = (n1: number, n2: number) => {
+    return n1 + n2
+}
 
-    describe(this: Department): void {
-        console.log(ItDepartment)
+console.log(add(3, 5))
 
-    }
+interface Named {
+    readonly name?: string
+    outputName?: string
+}
+
+interface Greetable extends Named{
+    greet(phrase: string): void
 }
 
 
-const ItDepartment = new ITDepartment('2', ['Max'])
-ItDepartment.describe()
-ItDepartment.printEmployeeInfo()
-ItDepartment.addEmployees('Max')
-ItDepartment.addEmployees('Alberto')
-console.log(ItDepartment)
-
-
-class accountingDepartment extends Department {
-
-    private lastReport: string
-    //SINGLETON PATRON
-    private static instance: accountingDepartment
-
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport
+class Person implements Greetable {
+    name?: string
+    age = 27
+    constructor(n?: string) {
+        if (n) {
+            this.name = n
         }
-        throw new Error('Report not found...')
     }
 
-    set mostRecentReport(value: string) {
-        if (!value) {
-            throw new Error('Valid value...')
+    greet(phrase: string) {
+        if (this.name) {
+            console.log(phrase + ' ' + this.name)
+        } else {
+            console.log('Hi!')
         }
-        this.addReports(value)
     }
 
-    private constructor(id: string, private reports: string[],) {
-        super(id, 'Account')
-        this.lastReport = reports[0]
-    }
-
-    static getInstance() {
-        if (accountingDepartment.instance) {
-            this.instance
-        }
-        this.instance = new accountingDepartment('2', [])
-        return this.instance
-    }
-
-    addEmployees(name: string) {
-        if (name === 'Alberto') {
-            return
-        }
-        this.people.push(name)
-    }
-
-    static createEmployee(name: string) {
-        return {name: name}
-    }
-
-    addReports(text: string) {
-        this.reports.push(text)
-        this.lastReport = text
-    }
-
-    printReports() {
-        console.log(this.reports)
-    }
-
-    describe(this: Department): void {
-        console.log(accountingDepartment)
-    }
+    //La interrogación indica que esa propiedad es opcional puede estar presente o no. Al crear la clase se le puede pasar un nombre o no.
 }
 
-//Singleton Patron
-const accountingD = accountingDepartment.getInstance()
-console.log(accountingD)
-
-const employee1 = accountingDepartment.createEmployee('Max')
-console.log('E1', employee1, Department.fiscalYear)
+let user1: Greetable
+user1 = new Person()
 
 
-accountingD.describe()
-accountingD.addReports('Semana 1')
-accountingD.addEmployees('Maximilian')
-accountingD.printEmployeeInfo()
-accountingD.mostRecentReport = ''
-//console.log(accountingD)
+user1.greet('Hi there - I am')
+console.error(user1)
 
+// let user2: Greetable
+// user2 = {
+//     name: 'Alexis',
+//     age: 27,
+//     greet(phrase: string) {
+//         alert(phrase + ' ' + this.name)
+//     },
+// }
 
-
-
-
-
-
+// user2.greet('Hi there - I am')
