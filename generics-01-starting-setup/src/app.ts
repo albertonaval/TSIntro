2386076cf6136c813c1820792f6f8e3397df30ca
albertonaval <albertonaval.dev@gmail.com>
2023-03-08 23:36:18 +0100

    Generic Function

--- generics-01-starting-setup/src/app.ts
+++ generics-01-starting-setup/src/app.ts
@@ -1,1 +1,20 @@
-// Code goes here!
+// const surnames: Array<string> = ['Hola', 'adios']
+// surnames[0].split(' ')
+
+// const promise: Promise <string> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('It is Done')
+//     }, 2000)
+// })
+
+// promise.then(data => {
+//     data.split(' ')
+// })
+
+
+function merge<U>(objA: {}, objB: U) {
+    return Object.assign(objA, objB)
+}
+
+const mergeObj = merge({ name: 'Alberto' }, { age: 20 })
+console.log(mergeObj.age)
